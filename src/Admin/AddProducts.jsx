1fc651adefc830a16e3c598827f2d2d124a9f489
DOSSA-HASAN <hassandossa5@gmail.com>
2024-09-24 12:02:02 +0300

    Adding on to AddProducts.jsx

--- src/Admin/AddProducts.jsx
+++ src/Admin/AddProducts.jsx
@@ -3,8 +3,8 @@ import Footer from '../footer/Footer'
 import { productsContext } from '../ProductsContext'
 import './addproducts.css'
 import NavBar from '../navbar/NavBar'
-import { setDoc } from 'firebase/firestore'
-import { storage } from '../firebaseconfig/FirebaseConfig'
+import { setDoc, doc } from 'firebase/firestore'
+import { storage, db } from '../firebaseconfig/FirebaseConfig'
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { nanoid } from 'nanoid'
 
@@ -12,13 +12,16 @@ function AddProducts() {
 
     const { furnitureItems } = useContext(productsContext);
 
-    const [productName, setProductName] =useState(null)
-    const [productType, setProductType] =useState(null)
-    const [productDescription, setProductDescription] =useState(null)
-    const [quantityAvailable, setQuantityavailable] =useState(null)
-    const [productWeight, setProductWeight] = useState(null)
-    const [productImage, setProductImage] = useState(null)
-    const [productDemoImage, setProductDemoImage] = useState(null)
+    const [productName, setProductName] =useState("")
+    const [productType, setProductType] =useState("")
+    const [productDescription, setProductDescription] =useState("")
+    const [quantityAvailable, setQuantityavailable] =useState("")
+    const [productWeight, setProductWeight] = useState("")
+    const [productImage, setProductImage] = useState("")
+    const [productDemoImage, setProductDemoImage] = useState("")
+
+    const [successMssg, setSuccessMssg] = useState('');
+    const [errorMssg, setErrorMssg] = useState('');
 
     useEffect(() => {
         if(productImage){
@@ -31,35 +34,77 @@ function AddProducts() {
     const handleAddProducts = async (e) => {
         e.preventDefault();
 
-        //adding img
-        const storageRef = ref(storage, `product-images`)
-        const snapshot = await uploadBytes(storageRef, productImage)
-        const imageURl = await getDownloadURL(snapshot.ref)
-        console.log(imageURl)
+        if(productImage){
+            const docID = nanoid()
 
-        await setDoc((db, "product-doc", nanoid), {
-            productName,
-            productType, 
-            productDescription,
-            quantityAvailable, 
-            productWeight,
-            image: imageURl
-        })
+            try {
+                //adding img
+                const storageRef = ref(storage, `product-images/${productType.toLowerCase() + docID}`)
+                const snapshot = await uploadBytes(storageRef, productImage)
+                const imageURl = await getDownloadURL(snapshot.ref)
+                console.log(imageURl)
+    
+                await setDoc(doc(db, `product-${productType}`, docID),{
+                    productName,
+                    productType, 
+                    productDescription,
+                    quantityAvailable, 
+                    productWeight,
+                    image: imageURl
+                })
+    
+                setSuccessMssg("Successfully added Product.")
+                setErrorMssg("")
+    
+            } catch (error) {
+                setErrorMssg(error.message)
+                setSuccessMssg("")
+            }
+            finally{
+                setProductName("")
+                setProductType("")
+                setProductDescription("")
+                setQuantityavailable("")
+                setProductWeight("")
+                setProductImage("")
+                setProductDemoImage("")
+            }
+        }
+        else{
+            setErrorMssg("Please fill all required fields")
+            setSuccessMssg("")
+        }
+
+        
+
+        
+
     }
 
     return (
         <>
         <NavBar />
         <section className='add-products-section m-auto'>
-            <form className='add-products-form'>
+            {successMssg && 
+                <div className="success-mssg-cont">
+                    <p>{successMssg}</p>
+                </div>
+            }
+
+            {errorMssg && 
+                <div className="error-mssg-cont">
+                    <p>{errorMssg}</p>
+                </div>
+            }
+            <form className='add-products-form' onSubmit={handleAddProducts}>
                 <span>
                     <label htmlFor="name">Product name</label>
-                    <input type="text" id='name' placeholder='Enter product name' required onChange={(e) => setProductName(e.target.value.toUpperCase())}/>
+                    <input type="text" id='name' value={productName} placeholder='Enter product name' required onChange={(e) => setProductName(e.target.value.toUpperCase())}/>
                 </span>
 
                 <span>
                     <label htmlFor="type">Product Type</label>
-                    <select id="type" required onChange={(e) => setProductType(e.target.value.toUpperCase())}>
+                    <select id="type" value={productType} required onChange={(e) => setProductType(e.target.value.toLowerCase())}>
                         <option value="" selected hidden>Select Product Type</option>
                         { furnitureItems.map((item, index) => (
                             <option value={item} key={index}>{item}</option>
@@ -69,32 +114,31 @@ function AddProducts() {
 
                 <span>
                     <label htmlFor="description">Description</label>
-                    <input type="text" id='description' placeholder='Enter product description' required onChange={(e) => setProductDescription(e.target.value.toUpperCase())}/>
+                    <input type="text" value={productDescription} id='description' placeholder='Enter product description' required onChange={(e) => setProductDescription(e.target.value.toUpperCase())}/>
                 </span>
 
                 <span>
                     <label htmlFor="quantity">Quantity Available</label>
-                    <input type="number" id='quantity' placeholder='Ente available quantity' required onChange={(e) => setQuantityavailable(e.target.value.toUpperCase())}/>
+                    <input type="number" value={quantityAvailable} id='quantity' placeholder='Ente available quantity' required onChange={(e) => setQuantityavailable(e.target.value.toUpperCase())}/>
                 </span>
 
                 <span>
                     <label htmlFor="weight">Product Weight</label>
-                    <input type="number" id='weight' placeholder='Enter product net weight' required onChange={(e) => setProductWeight(e.target.value)}/>
+                    <input type="number" value={productWeight} id='weight' placeholder='Enter product net weight' required onChange={(e) => setProductWeight(e.target.value)}/>
                 </span>
 
                 <span>
                     <label htmlFor="image">Product Image</label>
                     <input type="file" id='image' placeholder='Select Product Image' required onChange={(e) => setProductImage(e.target.files[0])}/>
                 </span>
-                {productDemoImage ?
+                {productDemoImage &&
                     <div>
                         <img src={productDemoImage} alt="" />
                     </div>
-                    :
-                    <div>
-                        <p>No product image currenlty selected</p>
-                    </div>
                 }
+                <div className="submit-btn-cont">
+                    <button type='submit'>Add Product</button>
+                </div>
             </form>
         </section>
         <Footer />
