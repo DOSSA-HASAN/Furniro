0104b997c62367846b8197b0b88ba8ed504f5c9a
DOSSA-HASAN <hassandossa5@gmail.com>
2024-10-06 15:33:29 +0300

    Updates to checkout page

--- src/shop/Checkout.jsx
+++ src/shop/Checkout.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useEffect, useState } from 'react'
 import NavBar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
 import { useParams } from 'react-router'
-import { doc, getDoc } from 'firebase/firestore'
+import { count, doc, getDoc } from 'firebase/firestore'
 import { db } from '../firebaseconfig/FirebaseConfig'
 import { productsContext } from '../ProductsContext'
+import './Hero.css'
 
 function Checkout() {
 
@@ -25,6 +26,21 @@ function Checkout() {
     const [email, setEmail] = useState("")
     const [additionalInfo, setAdditionalInfo] = useState("")
 
+    //set quantity
+    const [counter, setCounter] = useState(1)
+
+    const handleCounterIncrement = () => {
+        if(counter >= 1 && counter <= product.quantityAvailable){
+            setCounter(counter + 1)
+        }
+    }
+
+    const handleCounterDecrement = () => {
+        if(counter > 1){
+            setCounter(counter - 1)
+        }
+    }
+
 
     useEffect(() => {
         try {
@@ -32,12 +48,13 @@ function Checkout() {
                 const docRef = doc(db, "product", id)
                 const snapShot = await getDoc(docRef)
                 setProduct({...snapShot.data(), id:snapShot.id})
+                console.log(product)
             }
             fetchProduct()
         } catch (error) {
             console.log(error.message)
         }
-    },)
+    }, [])
 
 
     return (
@@ -46,16 +63,19 @@ function Checkout() {
             <section className='m-auto checkout-section'>
                 <article className='shipping-info-cont'>
                     <h1>Billing details</h1>
-                    <form action="">
-                        <span className='names-cont'>
-                            <label htmlFor="fname">First Name</label>
-                            <input type="text" id='fnam' onChange={(e) => setFirstName(e.target.value)} required />
-
-                            <label htmlFor="lname">Last Name</label>
-                            <input type="text" id='lname' onChange={(e) => setLastName(e.target.value)} required />
-                        </span>
+                    <form action="" className='billing-form'>
+                        <div className='names-cont'>
+                            <span>
+                                <label htmlFor="fname">First Name</label>
+                                <input type="text" value={firstName} id='fnam' onChange={(e) => setFirstName(e.target.value)} required />
+                            </span>
+                            <span>
+                                <label htmlFor="lname">Last Name</label>
+                                <input type="text" value={lastName} id='lname' onChange={(e) => setLastName(e.target.value)} required />
+                            </span>
+                        </div>
                         
-                        <span>
+                        <span className='country-cont'>
                             <label htmlFor="country">Country / Region</label>
                             <select id="country" onChange={(e) => setCountry(e.target.value)}>
                                 <option value="" selected hidden>Select Country</option>
@@ -87,9 +107,52 @@ function Checkout() {
 
                         <span>
                             <label htmlFor="phone">Phone</label>
-                            <input type="tel" id="phone" value={zipcode} onChange={(e) => setZipcode(e.target.value)}/>
+                            <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                        </span>
+
+                        <span>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </span>
+
+                        <span>
+                            <label htmlFor="add-info">Additional Information</label>
+                            <input type="text" value={additionalInfo} onChange={(e) => setAdditionalInfo(e.target.value)}/>
                         </span>
                     </form>
+                </article>
+
+                <article className='products-list-checkout-cont'>
+                    {product ?
+                    <>
+                        <span className='titles'>
+                            <h1>Product</h1>
+                            <h1>Subtotal</h1>
+                        </span>
+                        
+                        <span>
+                            <p>Quantity</p>
+                            <div className="quantity-cont">
+                                <button onClick={handleCounterIncrement}>+</button>
+                                <p>{counter}</p>
+                                <button onClick={handleCounterDecrement}>-</button>
+                            </div>
+                        </span>
+
+                        <span>
+                            <p>{product.productName} x {counter}</p>
+                            <p>{product.productPrice * counter} $</p>
+                        </span>
+
+                        <span className='total-price-cont'>
+                            <p>Total</p>
+                            <p>{product.productPrice * counter} $</p>
+                        </span>
+                        
+                    </> 
+                    :
+                        <p>Loading...</p>
+                    }
                 </article>
             </section>
             <Footer />
